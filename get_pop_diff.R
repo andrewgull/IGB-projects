a56547a1f6893ab3dcc3e8b554a53105fb2087e2
Andrei <guliaev@genebiology.ru>
2019-05-30 17:58:46 +0300

    calculate Hedrick's Gst, Nei's Gst, Jost's D, write tables and trees

--- get_pop_diff.R
+++ get_pop_diff.R
@@ -13,14 +13,17 @@ get_pop_diff <- function(filename){
   obj <- read.genalex(filename)
   pw.D <- pairwise_D(obj)
   pw.G <- pairwise_Gst_Hedrick(obj)
+  pw.GN <- pairwise_Gst_Nei(obj)
   
   pw.D.df <- as.data.frame(as.matrix(pw.D))
   pw.G.df <- as.data.frame(as.matrix(pw.G))
+  pw.GN.df <- as.data.frame(as.matrix(pw.GN))
   
   pw.D.df$measure <- rep("Jost's D", nrow(pw.D.df))
   pw.G.df$measure <- rep("Hedrick's Gst", nrow(pw.G.df))
+  pw.GN.df$measure <- rep("Nei's Gst", nrow(pw.GN.df))
   
-  out.df <- rbind(pw.G.df, rep('', ncol(pw.G.df)), pw.D.df)
+  out.df <- rbind(pw.G.df, rep('', ncol(pw.G.df)), pw.GN.df, rep('', ncol(pw.GN.df)), pw.D.df)
   write.table(out.df, 'pop_diff_table.csv', sep = '\t', row.names = T)
   
   # make and write trees
@@ -30,6 +33,9 @@ get_pop_diff <- function(filename){
   pw.G.tree <- nj(pw.G)
   write.tree(pw.G.tree, 'hedrick_G_tree.nwk')
   
+  pw.GN.tree <- nj(pw.GN)
+  write.tree(pw.GN.tree, 'nei_G_tree.nwk')
+  
   # write figures
   png(filename = 'jost_D_tree.png', width = 850, height = 640)
   plot(pw.D.tree)
@@ -37,6 +43,10 @@ get_pop_diff <- function(filename){
   
   png(filename = 'hedrick_G_tree.png', width = 850, height = 640)
   plot(pw.G.tree)
+  dev.off()
+  
+  png(filename = 'nei_G_tree.png', width = 850, height = 640)
+  plot(pw.GN.tree)
   dev.off()
   
 }
